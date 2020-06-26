c49318b941d623cbdd5f7d2485cb660e1338e6cf
herry <1033465081@qq.com>
2020-06-26 21:13:32 +0800

    extract github config from code to properties

---
+++ src/main/java/life/sgldts/community/controller/AuthorizeController.java
@@ -0,0 +1,54 @@
+package life.sgldts.community.controller;
+
+import com.sun.org.apache.bcel.internal.classfile.Code;
+import life.sgldts.community.dto.AccessTokenDTO;
+import life.sgldts.community.dto.GithubUser;
+import life.sgldts.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author herry
+ * @create 2020-06-26-17:10
+ */
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        } else {
+            return "redirect:/";
+        }
+    }
+}
